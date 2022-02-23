885c4b4902595c8dd5ef8dc650b329c5e44bb748
Vasini Lele <raffaelevasini@gmail.com>
2022-02-23 13:16:54 +0100

    config typeorm env file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
